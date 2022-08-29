e08bfba7e021a4b9a5dccdb9bb729907425e7078
João Xavier <joaoxneto7050@gmail.com>
2022-08-29 19:13:44 -0300

    up files

---
+++ diskstats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os
+import json
+
+if __name__ == "__main__":
+    # Iterate over all block devices, but ignore them if they are in the
+    # skippable set
+    skippable = ("sr", "loop", "ram")
+    devices = (device for device in os.listdir("/sys/class/block")
+               if not any(ignore in device for ignore in skippable))
+    data = [{"{#DEVICENAME}": device} for device in devices]
+    print(json.dumps({"data": data}, indent=4))
+[root@TAPAJOS-STBY-002 scripts]# cat diskstats.conf
+UserParameter=custom.vfs.discover_disks,/etc/zabbix/scripts/diskstats.py
+UserParameter=custom.vfs.dev.read.ops[*],awk '{print $$1}' /sys/class/block/$1/stat
+UserParameter=custom.vfs.dev.read.merged[*],awk '{print $$2}' /sys/class/block/$1/stat
+UserParameter=custom.vfs.dev.read.secto
